b237c87286629062163c6b08a7a1db89bcb00d7f
efoust11 <efoust11@ivytech.edu>
2026-02-17 19:23:02 -0500

    testing sql setup

--- HelloFX/src/hellofx/Main.java
+++ HelloFX/src/hellofx/Main.java
@@ -1,23 +1,32 @@
 package hellofx;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.sql.*;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
 
-public class Main extends Application {
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
-        primaryStage.show();
-    }
+public class Main{
+
+    //@Override
+    //public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 400, 300));
+        //primaryStage.show();
+    //}
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        //Class.forName("JDBCDriverClass");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/test","root","");
+        Statement statement = connection.createStatement();
+        statement.executeUpdate("create table Temp (col1 char(5), col2 char(5))");
+
     }
 }
